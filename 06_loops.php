cb3499c04ea9d141e8055128e7551435d08084cd
yaasiinaxmed <yaskassoy@gmail.com>
2025-08-25 21:00:44 +0300

    Add 06_loops.php with examples of while and foreach loops

---
+++ 06_loops.php
@@ -0,0 +1,21 @@
+<?php 
+
+// while loop
+
+$i = 1;
+
+while ($i <= 14) {
+    echo "$i \n" ;
+    $i++;
+}
+
+echo "<br>";
+
+// foreach loop
+
+$colors = array("red", "blue", "green", "yellow");
+
+foreach ($colors as $color) {
+    echo "$color \n";
+
+}
