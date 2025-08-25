1f4f257a12dceda488bd1a59a5ad642f209e34cb
yaasiinaxmed <yaskassoy@gmail.com>
2025-08-25 16:16:46 +0300

    Initial implementation of the project structure and basic functionality.

---
+++ 03_math.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo 5 * (6 + 3) - 1 . "\n";
+
+$x = 15;
+$y = 4;
+
+// addition
+echo $x + $y;
+echo "<br>";
+
+// subtraction
+echo $x - $y;
+echo "<br>";
+
+// multiplication
+echo $x * $y;
+echo "<br>";
+
+// division
+echo $x / $y;
+echo "<br>";
+
+echo 3 ** 2;
+echo "<br>";
+
+echo 10 % 3;
+echo "<br>";
+
+echo 5**2 * ( 6 + 3) - 1;
+echo "<br>";
