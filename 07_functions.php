4885f4eacc8c8146decf470d75d9705ea46c97bb
yaasiinaxmed <yaskassoy@gmail.com>
2025-08-27 06:23:25 +0300

    Add getLargestNumber function to 07_functions.php for comparing two numbers

--- 07_functions.php
+++ 07_functions.php
@@ -1,5 +1,11 @@
 <?php 
 
+function getLargestNumber( $a, $b ) {
+    return $a > $b ? $a : $b;
+}
+
+var_dump( getLargestNumber( 10, 20));
+
 function is_palindrome( $string ) {
     $string = str_replace(" ", "", $string );
     $string = strtolower($string);
